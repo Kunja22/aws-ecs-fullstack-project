ee6c76e8f68d07941471a0e3c1cda9a19f7fa994
Kunja Bhat <kunjabhat0@gmail.com>
2026-03-22 16:18:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-gfgcvhvhb
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from ECS backend!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
