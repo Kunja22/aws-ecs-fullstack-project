f69f17ce08fb8fade25029e8dfc1a0e8931767ec
Kunja Bhat <kunjabhat0@gmail.com>
2026-03-22 16:20:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,20 @@
-fyfygvh
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("http://YOUR-ALB-URL/api")
+      .then(res => res.json())
+      .then(data => setData(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>AWS DevOps Project 🚀</h1>
+      <p>{data}</p>
+    </div>
+  );
+}
+
+export default App;
